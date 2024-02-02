5c5434cd89ebfaf6b55b4bdd03d1ec7a62135811
Jenish <sudipkhatri036@gmail.com>
2024-02-02 15:26:53 +0545

    Added some animation

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const openMenu = document.getElementById("ham-menu");
 const closeMenu = document.getElementById("ham-close");
 const navMenu = document.getElementById("nav-bar");
+
 openMenu.addEventListener("click", () => {
   openMenu.style.display = "none";
   navMenu.style.display = "flex";
   closeMenu.style.display = "block";
+
+  gsap.from(closeMenu, {
+    y: -40,
+    duration: 1,
+    delay: 0.3,
+    opacity: 0,
+    ease: "bounce",
+  });
 });
 
 closeMenu.addEventListener("click", () => {
   closeMenu.style.display = "none";
   navMenu.style.display = "none";
   openMenu.style.display = "block";
+
+  gsap.from(openMenu, {
+    y: -40,
+    duration: 1,
+    delay: 0.3,
+    opacity: 0,
+    ease: "bounce",
+  });
 });
 
 //  scroll to top effect
@@ -21,7 +38,7 @@ window.onscroll = function () {
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
     scrollBtn.style.display = "flex";
   } else {
     scrollBtn.style.display = "none";
@@ -32,3 +49,35 @@ scrollBtn.addEventListener("click", scrollWin);
 function scrollWin() {
   window.scrollTo(0, 0);
 }
+
+// GSAP Library uses
+let tl = gsap.timeline();
+
+tl.from("header>h2", {
+  y: -15,
+  opacity: 0,
+  duration: 0.5,
+  delay: 0.5,
+});
+
+tl.from("#nav-bar>h3", {
+  y: -15,
+  opacity: 0,
+  duration: 0.5,
+  stagger: 0.3,
+});
+
+tl.from("#profile", {
+  x: -15,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.3,
+});
+
+gsap.from(".hero-content>h1 , .title, .sub-title", {
+  xPercent: 20,
+  opacity: 0,
+  duration: 0.5,
+  stagger: 0.3,
+  delay: 1,
+});
